e0b39681dd3c0f90a28ce8bf275e3f3c8522cb9f
RiddhiVora23 <114712420+RiddhiVora23@users.noreply.github.com>
2022-10-01 02:30:13 -0400

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author riddhivora
+ */
+public class employee {
+    private String name;
+    private int employeeID;
+    private int age;
+    private String gender;
+    private String startdate;
+    private String level;
+    private String teaminfo;
+    private String positiontitle;
+    private String cellphone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(int startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeaminfo() {
+        return teaminfo;
+    }
+
+    public void setTeaminfo(String teaminfo) {
+        this.teaminfo = teaminfo;
+    }
+
+    public String getPositiontitle() {
+        return positiontitle;
+    }
+
+    public void setPositiontitle(String positiontitle) {
+        this.positiontitle = positiontitle;
+    }
+
+    public int getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(int cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setStartDate(String startdate) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setCellPhone(String cellphone) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
---
+++ employeeHistory.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author riddhivora
+ */
+public class employeeHistory {
+    private ArrayList<employee> history;
+    
+    public employeeHistory(){
+        this.history = new ArrayList<employee>();
+        
+    }
+
+    public ArrayList<employee> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<employee> history) {
+        this.history = history;
+    }
+    
+    public employee addNewEmployee(){
+        
+        employee newEmployee = new employee();
+        history.add(newEmployee);
+        return newEmployee;
+        
+    }
+}
